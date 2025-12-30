a4663e0388d4f9f3d0da511b1122407832b12313
mahesh-molugu <molugumahesh345@gmail.com>
2025-12-30 15:08:01 +0530

    Update HomeController.java

--- src/main/java/com/fitmannation/controller/HomeController.java
+++ src/main/java/com/fitmannation/controller/HomeController.java
@@ -1,1 +1,13 @@
+package com.fitman.controller; 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Fitman Backend is up and running!";
+    }
+}
